777f612fa3e35700061cbc147afaf3a1c2459f9d
stuti-leetcode-submission <147951605+stuti-leetcode-submission@users.noreply.github.com>
2023-10-28 14:12:47 +0530

    Time: 7 ms (51.29%) | Memory: 14.7 MB (5.47%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,9 +1,44 @@
 class Solution {
 public:
+    int lb1(vector<int>nums,int target){
+        int low=0,high=nums.size()-1,ans=-1;
+    //    if(nums[0]==target)return 0;
+    //    if(nums[nums.size()-1]<target)return nums.size();
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                high=mid-1;
+                ans=mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
+     int ub1(vector<int>nums,int target){
+        int low=0,high=nums.size()-1,ans=-1;
+        //if(nums[0]==target)return 0;
+    //    if(nums[nums.size()-1]==target)return nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                low=mid+1;
+                ans=mid;
+            }
+            else if(nums[mid]>target){
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
+    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        int lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
-        int ub=upper_bound(nums.begin(),nums.end(),target)-nums.begin()-1;
-        if(lb==-1 || lb==nums.size() || nums[lb]!=target || nums[ub]!=target)return {-1,-1};
+        int lb=lb1(nums,target);
+          int ub=ub1(nums,target);
+      ub;
+        if(lb==-1 || lb==nums.size() || ub==-1 || nums[lb]!=target || nums[ub]!=target)return {-1,-1};
         else return {lb,ub};
     }
 };
